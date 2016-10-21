7e1b96e85d523405c3c04b6191b136b9fb55c144
Karun Singh <karun.singh17@gmail.com>
2016-10-21 02:50:54 -0400

    Added speed simulation (0-99)

--- init.py
+++ init.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from Tkinter import *
+import ttk
+from time import sleep
+from threading import Thread
 
 class AppDriver(Tk):
 
@@ -13,6 +16,10 @@ class AppDriver(Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        self.speed = StringVar()
+        container.speed = self.speed
+        self.speed.set("0 kmph")
+
         self.frames = {}
         for F in (Welcome, Dash):
             page_name = F.__name__
@@ -23,20 +30,38 @@ class AppDriver(Tk):
         self.show_frame("Welcome")
 
     def show_frame(self, page_name):
-        print "showing "+page_name
         frame = self.frames[page_name]
         frame.tkraise()
+        # Begin simulation
+        if (page_name == "Dash"):
+            thread = Thread(target=self.speed_test)
+            thread.start()
+
+    def speed_test(self):
+        for i in range(100):
+            sleep(0.07)
+            self.speed.set(str(i)+" kmph")
 
 
 class Dash(Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent, background="#0f0")            
+        Frame.__init__(self, parent, background="#000")            
         self.parent = parent
         self.controller = controller
+        self.speed = self.parent.speed
         self.initUI()
 
     def initUI(self):
+        """pb_hd = ttk.Progressbar(self, orient='horizontal', mode='determinate')
+        pb_hd.pack(side=TOP)
+        pb_hd.step(50)"""
+        w = Label(self, textvariable=self.speed, font=("System", 30), fg="#fff", bg="#000", height=4)
+        w.pack();
+        b = Button(self, text="Back", command=lambda: self.controller.show_frame("Welcome"), bd=0,
+            bg="#f00", fg="#fff", height=40)
+        b.pack(side=TOP, fill=X)
         self.pack()
+        
 
 class Welcome(Frame):
   
